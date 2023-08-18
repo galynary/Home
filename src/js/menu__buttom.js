a76ffb7fb5f96a5ab1d2597bb8e8047947e658ca
galynary <107271972+galynary@users.noreply.github.com>
2023-08-18 16:27:49 +0300

    change

---
+++ src/js/menu__buttom.js
@@ -0,0 +1,21 @@
+const toggleButton = document.querySelector('#toggleButton');
+const slideLeft = document.querySelector('.wrap__left-slide');
+const slideRight = document.querySelector('.wrap__rigth-slide');
+
+let rotatedButton = false;
+
+toggleButton.addEventListener('click', () => {
+  if (!rotatedButton) {
+    toggleButton.style.transform = 'rotate(90deg)';
+    slideLeft.style.transform = 'translateX(-100%)';
+    slideRight.style.transform = 'translateX(100%)';
+  } else {
+    toggleButton.style.transform = '';
+    slideLeft.style.transform = '';
+    slideRight.style.transform = '';
+  }
+  slideLeft.style.transition = 'transform 0.7s';
+  slideRight.style.transition = 'transform 0.7s';
+
+  rotatedButton = !rotatedButton;
+});
