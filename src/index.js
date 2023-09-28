8531d024cc270761769d682e21632e960e619c9a
galynary <107271972+galynary@users.noreply.github.com>
2023-09-28 10:14:19 +0300

    CHANGE

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import { slider_mob1 } from './js/slider_mob1';
 import { contacts } from './js/contacts';
 import { about_us } from './js/about_us';
 import { page_open } from './js/page_open';
-/*import { selling } from './js/selling';*/
-import { faq } from './js/faq';
-import { discover_house } from './js/discover_house';
-import { description_house } from './js/description_house';
+/*import { faq } from './js/faq';
+import { discover_house } from './js/discover_house';*/
+/*import { description_house } from './js/description_house';*/
 
 
 
--- src/js/page_open.js
+++ src/js/page_open.js
@@ -5,62 +5,72 @@ const rightContainer = document.querySelector('.page-container__right');
 const menuButton = document.querySelector('.icon__menu-button2');
 const menuButtonItems = document.querySelectorAll('.menu-button__item');
 const openSelling = document.querySelector('[data-open-selling]');
-const page = document.querySelector('[data-selling]');
-const pageSection = document.querySelector('.wrap__page-section');
+const page = document.querySelector('.slide__page');
+const pageSection = document.querySelector('.page__section');
+const mySwiper = document.querySelector('.my__swiper');
 
-// Функція для обробки кліків на кнопки menuButtonItems
-function handleMenuButtonClick() {
-  toggleButton.style.display = 'none';
-  toggleButton2.style.display = 'block';
-  page.style.display = 'block';
-  page.style.zIndex = '40';
+menuButtonItems.forEach(item => {
+  item.addEventListener('click', () => {
+    toggleButton.style.display = 'none';
+    toggleButton2.style.display = 'block';
+    mySwiper.style.display = 'block';
+    mySwiper.style.zIndex = "40";
+   
+ //появляється page
   setTimeout(() => {
-    // leftContainer та rightContainer стають на позицію 0
-    menuButton.style.transform = 'rotate(180deg)';
-    toggleButton2.style.transform = 'translate(0%, -350%)';
-    leftContainer.style.transform = 'translateX(0%)';
-    rightContainer.style.transform = 'translateX(0%)';
-    leftContainer.style.transition = 'transform 6s ease';
-    rightContainer.style.transition = 'transform 6s ease';
-    toggleButton2.style.transition = 'transform 6s ease';
-  }, 1000);
-  // Появляється pageSection
-  setTimeout(() => {
-    pageSection.classList.add('appear');
-    pageSection.classList.remove('disappear');
-    pageSection.style.zIndex = '41';
-    toggleButton2.style.transition = 'transform 4s ease';
-  }, 6000);
-}
-
-// Додаємо обробник кліків для кожного елементу menuButtonItems
-menuButtonItems.forEach((item) => {
-  item.addEventListener('click', handleMenuButtonClick);
+        page.style.display = 'block';
+        page.style.zIndex = "40";
+        page.style.transition = 'transform 4s ease';
+      }, 300);
+       //leftContainer rightContainer стають на позицію 0
+      setTimeout(() => {
+      menuButton.style.transform = 'rotate(180deg)';
+      toggleButton2.style.transform = 'translate(0%, -350%)';
+      leftContainer.style.transform = 'translateX(0%)';
+      rightContainer.style.transform = 'translateX(0%)';
+      leftContainer.style.transition = 'transform 4s ease';
+      rightContainer.style.transition = 'transform 4s ease';
+      toggleButton2.style.transition = 'transform 4s ease';
+  },500);
+   //появляється pageSection
+    setTimeout(() => {
+       pageSection.style.display = 'block';
+       pageSection.style.zIndex = "41";
+       pageSection.style.animation= 'openPage 4s linear forwards';
+       pageSection.style.transition = 'transform 5s ease';
+    }, 2500);
+  });
 });
 
-let isOpenPage = true;
 
-// Обробник кліку на toggleButton2
+let isOpenPage = false;
+
+
 toggleButton2.addEventListener('click', () => {
-  // Зникає pageSection
- pageSection.classList.add('disappear');
-    pageSection.classList.remove('appear');
-    pageSection.style.transition = 'opacity 4s ease'; // Fixed transition property
- 
-  // leftContainer та rightContainer розсуваються
-  setTimeout(() => {
-  toggleButton2.style.transform = 'translate(-50%, 0%)';
-  menuButton.style.transform = 'rotate(90deg)';
+  pageSection.style.animation= 'closePage 4s linear forwards';
+  pageSection.style.transition = 'transform 5s ease';
+      //зникає pageSection
+    setTimeout(() => {
+      pageSection.style.display = 'none';
+      pageSection.style.transition = 'transform 5s ease';
+    }, 4000);
+       //leftContainer rightContainer розсуваються
+    setTimeout(() => {
+    menuButton.style.transform = 'rotate(90deg)';
+    toggleButton2.style.transform = 'translate(-50%, 0%)';
   leftContainer.style.transform = 'translateX(-100%)';
-  rightContainer.style.transform = 'translateX(100%)';
-  leftContainer.style.transition = 'transform 6s ease';
-  rightContainer.style.transition = 'transform 6s ease';
-  toggleButton2.style.transition = 'transform 6s ease';
-}, 2500);
-  setTimeout(() => {
-    page.style.display = 'none';
-    page.style.zIndex = '1';
-    toggleButton2.style.display = 'none';
-    toggleButton.style.display = 'block';
-  }, 10000);
-});
+    rightContainer.style.transform = 'translateX(100%)';
+    leftContainer.style.transition = 'transform 4s ease';
+    rightContainer.style.transition = 'transform 4s ease';
+    toggleButton2.style.transition = 'transform 4s ease';
+  }, 2000);
+    setTimeout(() => {
+    /*  page.style.display = 'none';
+      page.style.zIndex = "1";*/
+      mySwiper.style.display = 'none';
+      mySwiper.style.zIndex = "1";
+      toggleButton2.style.display = 'none';
+      toggleButton.style.display = 'block';
+    }, 6000);
+ 
+  })
